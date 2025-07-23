6d0dd2deae9c93ee50a1e746443f1e1159665afc
Sergey.Ryabkov <Sergey.Ryabkov@russianpost.ru>
2025-07-23 18:31:34 +0300

    add json output

--- cmd/xlsx-merger/main.go
+++ cmd/xlsx-merger/main.go
@@ -1,25 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/ryabkov82/xlsx-merger/internal/config"
 	"github.com/ryabkov82/xlsx-merger/internal/merger"
 )
 
+type Output struct {
+	Success     bool     `json:"success"`
+	OutputFiles []string `json:"output_files,omitempty"`
+	Error       string   `json:"error,omitempty"`
+}
+
 func main() {
 
 	cfg, err := config.ParseFlags()
 	if err != nil {
-		log.Fatalf("Ошибка конфигурации: %v", err)
+		emitJSON(Output{
+			Success: false,
+			Error:   fmt.Sprintf("Ошибка конфигурации: %v", err),
+		})
+		return
 	}
 
 	m := merger.NewStreamMerger()
 	outputFiles, err := m.MergeFiles(cfg)
 	if err != nil {
-		log.Fatalf("Ошибка объединения: %v", err)
+		emitJSON(Output{
+			Success: false,
+			Error:   fmt.Sprintf("Ошибка объединения: %v", err),
+		})
+		return
 	}
 
-	fmt.Printf("Файлы успешно объединены в %s\n", outputFiles)
+	emitJSON(Output{
+		Success:     true,
+		OutputFiles: outputFiles,
+	})
+
+}
+
+func emitJSON(out Output) {
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ") // для красивого вывода (опционально)
+	if err := enc.Encode(out); err != nil {
+		log.Fatalf("Ошибка вывода JSON: %v", err)
+	}
 }
